5becf4ecc92d27293cda29f15b54a6f5cd6c64a4
FASfalcon <alexanderfedasov@gmail.com>
2020-06-10 22:24:23 +0300

    MT update1.1 - добавление класса Scanner.

--- src/com/dragon/MultiplicationTable.java
+++ src/com/dragon/MultiplicationTable.java
@@ -3,14 +3,98 @@ package com.dragon;
 /*
  */
 
+import java.util.Scanner;
+
 public class MultiplicationTable {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.println(i + " * " + j + " = " + (i * j) + " ");
-            }
-            System.out.println();
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        switch (number) {
+            case 1:
+                for (int i = 1; i <= 1; i++) {
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " * " + j + " = " + (i * j) + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 2:
+                for (int i = 2; i <= 2; i++) {
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " * " + j + " = " + (i * j) + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 3:
+                for (int i = 3; i <= 3; i++) {
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " * " + j + " = " + (i * j) + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 4:
+                for (int i = 4; i <= 4; i++) {
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " * " + j + " = " + (i * j) + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 5:
+                for (int i = 5; i <= 5; i++) {
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " * " + j + " = " + (i * j) + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 6:
+                for (int i = 6; i <= 6; i++) {
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " * " + j + " = " + (i * j) + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 7:
+                for (int i = 7; i <= 7; i++) {
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " * " + j + " = " + (i * j) + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 8:
+                for (int i = 8; i <= 8; i++) {
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " * " + j + " = " + (i * j) + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 9:
+                for (int i = 9; i <= 9; i++) {
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " * " + j + " = " + (i * j) + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            case 10:
+                for (int i = 10; i <= 10; i++) {
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " * " + j + " = " + (i * j) + " ");
+                    }
+                    System.out.println();
+                }
+                break;
+            default:
+                System.out.println("Вы не попали в числа от 1 до 10, попробуйте снова :)");
+                break;
         }
     }
 }
